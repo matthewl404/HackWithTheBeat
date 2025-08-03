e8e50eecd4a9ed15bef7dc85e564e3310efc516e
jujai <49623751+jujai@users.noreply.github.com>
2025-08-03 18:15:57 -0400

    Update app.js
    removed .trim

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function onYouTubeIframeAPIReady() {
 // Game Functions
 async function startGame() {
   const btn = document.getElementById('start-with-transcript');
-  const transcript = document.getElementById('user-transcript').value.trim();
+  const transcript = document.getElementById('user-transcript').value.();
   
   showSpinner(btn);
   btn.textContent = 'Processing...';
